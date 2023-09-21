fe874f5da59127e3c9d5b09367575cebb33b3a5b
Alerdo <alerdo23@gmail.com>
2023-09-21 20:01:27 +0100

    Add route api call handler index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
-import  express  from 'express';
-import authenticateRouter from './authenticate.js';
-const router = express.Router();
-// import User from '../models/user.js'
-// import { authenticateRouter,User, Product, Cart, CartItem, Order, OrderItem }  from '../models/index.js';
 
+//Index.js serves as a constructor for all of the routes, in order for them to be later exported into the /setup/index.js file , where they get the passport argument.
+
+import authenticateRouter from './authentication';
+import userRouter from './user.js';
+import cartRouter from './cart';
+import cartItemRouter from './cartItem';
+import productRoutes from './product';
+import orderRoutes from './order';
+import orderItemRoutes from './orderItem';
 
 export default (app, passport) => {
+  // Initialize the different routes
   authenticateRouter(app, passport);
-  // cartRouter(app);
-  // orderRouter(app);
-  // productRouter(app);
-  // userRouter(app);
-}
+  userRouter(app, passport);
+  cartRouter(app, passport);
+  cartItemRouter(app, passport);
+  productRoutes(app, passport);
+  orderRoutes(app, passport);
+  orderItemRoutes(app, passport);
+};
