f65748273b6e11f8d05970c9e086b96e7d561381
Alerdo <alerdo23@gmail.com>
2023-09-29 20:44:46 +0100

    Add config files.

--- config/config.js
+++ config/config.js
@@ -2,6 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+let databaseConfig;
+if (process.env.PLATFORM_RELATIONSHIPS) {
+    const relationships = JSON.parse(Buffer.from(process.env.PLATFORM_RELATIONSHIPS, 'base64').toString());
+    databaseConfig = relationships.database[0];
+}
+
 const config = {
   development: {
     username: process.env.DEVELOPMENT_DB_USERNAME,
@@ -18,11 +24,12 @@ const config = {
     dialect: process.env.TEST_DB_DIALECT
   },
   production: {
-    username: process.env.PRODUCTION_DB_USERNAME,
-    password: process.env.PRODUCTION_DB_PASSWORD,
-    database: process.env.PRODUCTION_DB_NAME,
-    host: process.env.PRODUCTION_DB_HOST,
-    dialect: process.env.PRODUCTION_DB_DIALECT
+    username: databaseConfig ? databaseConfig.username : process.env.PRODUCTION_DB_USERNAME,
+    password: databaseConfig ? databaseConfig.password : process.env.PRODUCTION_DB_PASSWORD,
+    database: databaseConfig ? databaseConfig.path : process.env.PRODUCTION_DB_NAME,
+    host: databaseConfig ? databaseConfig.host : process.env.PRODUCTION_DB_HOST,
+    dialect: 'postgres', // Assuming it's always PostgreSQL for production on Platform.sh
+    port: databaseConfig ? databaseConfig.port : undefined
   }
 };
 
