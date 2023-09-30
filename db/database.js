90d23851f1a82501d51232ba24b0bbe653cf733c
Alerdo <alerdo23@gmail.com>
2023-10-01 00:19:07 +0100

    Fix sequelize declaration and other potential issues

--- db/database.js
+++ db/database.js
@@ -26,46 +26,46 @@ const sequelize = new Sequelize(process.env.DATABASE_URL || config.database, {
     }
 });
 
-// Initialize models
-User.initialize(sequelize, Sequelize.DataTypes);
-Product.initialize(sequelize, Sequelize.DataTypes);
-Order.initialize(sequelize, Sequelize.DataTypes);
-Cart.initialize(sequelize, Sequelize.DataTypes);
-CartItem.initialize(sequelize, Sequelize.DataTypes);
-OrderItem.initialize(sequelize, Sequelize.DataTypes);
+// // Initialize models
+// User.initialize(sequelize, Sequelize.DataTypes);
+// Product.initialize(sequelize, Sequelize.DataTypes);
+// Order.initialize(sequelize, Sequelize.DataTypes);
+// Cart.initialize(sequelize, Sequelize.DataTypes);
+// CartItem.initialize(sequelize, Sequelize.DataTypes);
+// OrderItem.initialize(sequelize, Sequelize.DataTypes);
 
-// User relationships
-User.hasMany(Order, { foreignKey: 'user_id', as: 'orders' });
-User.hasOne(Cart, { foreignKey: 'user_id', as: 'cart' });
+// // User relationships
+// User.hasMany(Order, { foreignKey: 'user_id', as: 'orders' });
+// User.hasOne(Cart, { foreignKey: 'user_id', as: 'cart' });
 
-// Cart relationships
-Cart.belongsTo(User, { foreignKey: 'user_id' });
-Cart.hasMany(CartItem, { foreignKey: 'cart_id', as: 'items' });
+// // Cart relationships
+// Cart.belongsTo(User, { foreignKey: 'user_id' });
+// Cart.hasMany(CartItem, { foreignKey: 'cart_id', as: 'items' });
 
-// Order relationships
-Order.belongsTo(User, { foreignKey: 'user_id' });
-Order.hasMany(OrderItem, { foreignKey: 'order_id', as: 'items' });
+// // Order relationships
+// Order.belongsTo(User, { foreignKey: 'user_id' });
+// Order.hasMany(OrderItem, { foreignKey: 'order_id', as: 'items' });
 
-// Product relationships
-Product.belongsToMany(Cart, { through: CartItem, foreignKey: 'product_id' });
-Product.belongsToMany(Order, { through: OrderItem, foreignKey: 'product_id' });
+// // Product relationships
+// Product.belongsToMany(Cart, { through: CartItem, foreignKey: 'product_id' });
+// Product.belongsToMany(Order, { through: OrderItem, foreignKey: 'product_id' });
 
-// CartItem relationships
-CartItem.belongsTo(Cart, { foreignKey: 'cart_id' });
-CartItem.belongsTo(Product, { foreignKey: 'product_id' });
+// // CartItem relationships
+// CartItem.belongsTo(Cart, { foreignKey: 'cart_id' });
+// CartItem.belongsTo(Product, { foreignKey: 'product_id' });
 
-// OrderItem relationships
-OrderItem.belongsTo(Order, { foreignKey: 'order_id' });
-OrderItem.belongsTo(Product, { foreignKey: 'product_id' });
+// // OrderItem relationships
+// OrderItem.belongsTo(Order, { foreignKey: 'order_id' });
+// OrderItem.belongsTo(Product, { foreignKey: 'product_id' });
 
-// Check database connection
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(error => {
-    console.error('Unable to connect to the database:', error);
-  });
+// // Check database connection
+// sequelize.authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(error => {
+//     console.error('Unable to connect to the database:', error);
+//   });
 
 const db = {
   sequelize,
@@ -79,4 +79,4 @@ const db = {
 };
 
 export default db;
-export { sequelize };
+
