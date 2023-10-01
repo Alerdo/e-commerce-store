a504b4e1b3e68f2e94a5a98ba99a252ec092d52e
Alerdo <alerdo23@gmail.com>
2023-10-01 19:18:44 +0100

    Setup heroku

--- db/database.js
+++ db/database.js
@@ -33,45 +33,36 @@ CartItem.initialize(sequelize, Sequelize.DataTypes);
 OrderItem.initialize(sequelize, Sequelize.DataTypes);
 
 
-// // User relationships
-// User.hasMany(Order, { foreignKey: 'user_id', as: 'orders' });
-// User.hasOne(Cart, { foreignKey: 'user_id', as: 'cart' });
+// User relationships
+User.hasMany(Order, { foreignKey: 'user_id', as: 'orders' });
+User.hasOne(Cart, { foreignKey: 'user_id', as: 'cart' });
 
-// // Cart relationships
-// Cart.belongsTo(User, { foreignKey: 'user_id' });
-// Cart.hasMany(CartItem, { foreignKey: 'cart_id', as: 'items' });
+// Cart relationships
+Cart.belongsTo(User, { foreignKey: 'user_id' });
+Cart.hasMany(CartItem, { foreignKey: 'cart_id', as: 'items' });
 
-// // Order relationships
-// Order.belongsTo(User, { foreignKey: 'user_id' });
-// Order.hasMany(OrderItem, { foreignKey: 'order_id', as: 'items' });
+// Order relationships
+Order.belongsTo(User, { foreignKey: 'user_id' });
+Order.hasMany(OrderItem, { foreignKey: 'order_id', as: 'items' });
 
-// // Product relationships
-// Product.belongsToMany(Cart, { through: CartItem, foreignKey: 'product_id' });
-// Product.belongsToMany(Order, { through: OrderItem, foreignKey: 'product_id' });
+// Product relationships
+Product.belongsToMany(Cart, { through: CartItem, foreignKey: 'product_id' });
+Product.belongsToMany(Order, { through: OrderItem, foreignKey: 'product_id' });
 
-// // CartItem relationships
-// CartItem.belongsTo(Cart, { foreignKey: 'cart_id' });
-// CartItem.belongsTo(Product, { foreignKey: 'product_id' });
+// CartItem relationships
+CartItem.belongsTo(Cart, { foreignKey: 'cart_id' });
+CartItem.belongsTo(Product, { foreignKey: 'product_id' });
 
-// // OrderItem relationships
-// OrderItem.belongsTo(Order, { foreignKey: 'order_id' });
-// OrderItem.belongsTo(Product, { foreignKey: 'product_id' });
+// OrderItem relationships
+OrderItem.belongsTo(Order, { foreignKey: 'order_id' });
+OrderItem.belongsTo(Product, { foreignKey: 'product_id' });
 
-// // Check database connection
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(error => {
-//     console. error('Unable to connect to the database:', error);
-//   });
+// Check database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(error => {
+    console. error('Unable to connect to the database:', error);
+  });
 
-// export default db = {
-// //   Sequelize,
-//   User,
-//   Product,
-//   Order,
-//   Cart,
-//   CartItem,
-//   OrderItem
-// };
