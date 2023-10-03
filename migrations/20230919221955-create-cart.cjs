550fff9e2615c60911e2d3831f031456802faf55
Alerdo <alerdo23@gmail.com>
2023-10-03 19:34:36 +0100

    Change node version o pck.json

--- migrations/20230919221955-create-cart.cjs
+++ migrations/20230919221955-create-cart.cjs
@@ -1,28 +1,30 @@
-export const up =  async (queryInterface, Sequelize) => {
-  await queryInterface.createTable('carts', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    user_id: {
-      type: Sequelize.INTEGER
-    },
-    creation_date: {
-      type: Sequelize.DATE
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  });
-};
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    await queryInterface.createTable('carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      creation_date: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
 
-export const down = async (queryInterface) => {
-  await queryInterface.dropTable('carts');
+  down: async function (queryInterface) {
+    await queryInterface.dropTable('carts');
+  }
 };
--- migrations/20230919222004-create-cart-item.cjs
+++ migrations/20230919222004-create-cart-item.cjs
@@ -1,31 +1,33 @@
-export const up =  async (queryInterface, Sequelize) => {
-  await queryInterface.createTable('cart_items', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    cart_id: {
-      type: Sequelize.INTEGER
-    },
-    product_id: {
-      type: Sequelize.INTEGER
-    },
-    quantity: {
-      type: Sequelize.INTEGER
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  });
-};
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    await queryInterface.createTable('cart_items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      cart_id: {
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
 
-export const down = async (queryInterface) => {
-  await queryInterface.dropTable('cart_items');
+  down: async function (queryInterface) {
+    await queryInterface.dropTable('cart_items');
+  }
 };
