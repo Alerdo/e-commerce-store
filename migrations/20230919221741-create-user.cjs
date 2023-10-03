5955ebd6e092ef9bebd68ac9b11350d70d248aff
Alerdo <alerdo23@gmail.com>
2023-10-03 19:29:51 +0100

    Try common js at the migrations files

--- migrations/20230919221741-create-user.cjs
+++ migrations/20230919221741-create-user.cjs
@@ -1,39 +1,41 @@
-export const up =  async (queryInterface, Sequelize) => {
-  await queryInterface.createTable('users', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    email: {
-      allowNull: false,
-      type: Sequelize.STRING,
-      unique: true
-    },
-    password: {
-      type: Sequelize.STRING
-    },
-    name: {
-      type: Sequelize.STRING
-    },
-    address: {
-      type: Sequelize.STRING
-    },
-    registration_date: {
-      type: Sequelize.DATE
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  });
-};
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      registration_date: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
 
-export const down = async (queryInterface) => {
-  await queryInterface.dropTable('users');
+  down: async function (queryInterface) {
+    await queryInterface.dropTable('users');
+  }
 };
--- migrations/20230919221853-create-product.cjs
+++ migrations/20230919221853-create-product.cjs
@@ -1,40 +1,42 @@
-export const up =  async (queryInterface, Sequelize) => {
-  await queryInterface.createTable('products', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    name: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
-    description: {
-      type: Sequelize.TEXT
-    },
-    price: {
-      allowNull: false,
-      type: Sequelize.DECIMAL(10, 2)
-    },
-    stock_quantity: {
-      allowNull: false,
-      type: Sequelize.INTEGER
-    },
-    image_url: {
-      type: Sequelize.STRING
-    },
-    created_at: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updated_at: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  });
-};
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL(10, 2)
+      },
+      stock_quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      image_url: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
 
-export const down = async (queryInterface) => {
-  await queryInterface.dropTable('products');
+  down: async function (queryInterface) {
+    await queryInterface.dropTable('products');
+  }
 };
