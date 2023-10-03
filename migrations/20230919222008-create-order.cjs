3264c919cf6ec7c03e0241411baf20d1ff47b37d
Alerdo <alerdo23@gmail.com>
2023-10-03 19:32:49 +0100

    Try common js at the migrations files

--- migrations/20230919222008-create-order.cjs
+++ migrations/20230919222008-create-order.cjs
@@ -1,34 +1,36 @@
-export const up = async (queryInterface, Sequelize) => {
-  await queryInterface.createTable('orders', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    user_id: {
-      type: Sequelize.INTEGER
-    },
-    order_date: {
-      type: Sequelize.DATE
-    },
-    shipping_address: {
-      type: Sequelize.STRING
-    },
-    status: {
-      type: Sequelize.STRING
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  });
-};
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      order_date: {
+        type: Sequelize.DATE
+      },
+      shipping_address: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
 
-export const down =  async (queryInterface) => {
-  await queryInterface.dropTable('orders');
+  down: async function (queryInterface) {
+    await queryInterface.dropTable('orders');
+  }
 };
--- migrations/20230919222014-create-order-item.cjs
+++ migrations/20230919222014-create-order-item.cjs
@@ -1,34 +1,36 @@
-export const up =  async (queryInterface, Sequelize) => {
-  await queryInterface.createTable('order_items', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    order_id: {
-      type: Sequelize.INTEGER
-    },
-    product_id: {
-      type: Sequelize.INTEGER
-    },
-    quantity: {
-      type: Sequelize.INTEGER
-    },
-    price_at_time_of_purchase: {
-      type: Sequelize.DECIMAL
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  });
-};
+module.exports = {
+  up: async function (queryInterface, Sequelize) {
+    await queryInterface.createTable('order_items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      order_id: {
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      price_at_time_of_purchase: {
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
 
-export const down = async (queryInterface) => {
-  await queryInterface.dropTable('order_items');
+  down: async function (queryInterface) {
+    await queryInterface.dropTable('order_items');
+  }
 };
