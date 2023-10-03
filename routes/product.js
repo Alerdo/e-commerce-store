4f80ff077977a5da5ab368db8ffbc7c0091edf6b
Alerdo <alerdo23@gmail.com>
2023-10-03 23:02:07 +0100

    Add route handler to create a single product

--- routes/product.js
+++ routes/product.js
@@ -37,17 +37,46 @@ export default (app, passport) => {
 
 
 // ... other imports and code
+router.post('/create', async (req, res) => {
+    const { name, description, price, stock_quantity, image_url } = req.body;
+
+    // Basic validation: Check if all the required fields are provided
+    if (!name || !description || price == null || stock_quantity == null || !image_url) {
+        return res.status(400).json({ message: 'Please provide all required fields.' });
+    }
+
+    try {
+        const newProduct = await Product.create({
+            name,
+            description,
+            price,
+            stock_quantity,
+            image_url
+        });
+
+        // Return the created product
+        res.status(201).json(newProduct);
+
+    } catch (error) {
+        res.status(500).json({ 
+            errorName: error.name, 
+            errorMessage: error.message, 
+            errorStack: process.env.NODE_ENV === 'development' ? error.stack : undefined
+        });
+    }
+});
+
 
 router.post('/bulk', async (req, res) => {
-    const productsArray = req.body;
+    const {products} = req.body;
 
     // Validation: Check if productsArray is an array and has content
-    if (!Array.isArray(productsArray) || productsArray.length === 0) {
+    if (!Array.isArray(products) || products.length === 0) {
         return res.status(400).json({ message: 'Please send a valid array of products.' });
     }
 
     try {
-        const createdProducts = await Product.bulkCreate(productsArray);
+        const createdProducts = await Product.bulkCreate(products);
 
         // Return the created products
         res.status(201).json(createdProducts);
