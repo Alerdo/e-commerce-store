a1796eed3867e32b8202d2d5bea19eb96918c5f0
Alerdo <alerdo23@gmail.com>
2023-10-30 13:00:28 +0000

    Fix caritems

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 import express from 'express';
 import loaders from './setup/index.js';
-
 import dotenv from 'dotenv';
 
+dotenv.config(); // It's good to initialize dotenv at the top
 
 const PORT = process.env.PORT || 3001;
 
-
 const app = express();
 
-//For env sensitive info
-dotenv.config();
-
 const startServer = async () => {
-  // Init application loaders
-  loaders(app);
+  try {
+    loaders(app);
 
-  // Start server
-  app.listen(PORT, () => {
-    console.log(`Server listening on PORT ${PORT}`);
-  });
+    app.listen(PORT, () => {
+      console.log(`Server listening on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    process.exit(1); // Exit with a failure mode
+  }
 };
 
 startServer();
